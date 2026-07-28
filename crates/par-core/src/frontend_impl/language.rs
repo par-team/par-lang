2342f5acaed23b499ab748bebe43ad3c36fb2a24
noam <37066741+NoamDev@users.noreply.github.com>
2026-07-28 21:01:58 +0300

    Add syntax for `todo`

--- crates/par-core/src/frontend_impl/language.rs
+++ crates/par-core/src/frontend_impl/language.rs
@@ -439,6 +439,7 @@ pub enum Expression<S> {
     },
     Construction(Span, Construct<S>),
     Application(Span, Box<Self>, Apply<S>),
+    ToDo(Span),
 }
 
 #[derive(Clone, Debug)]
@@ -572,6 +573,7 @@ pub enum ProcessStep<S> {
         else_: Option<Box<Process<S>>>,
     },
     Command(ProcessCommand<S>),
+    ToDo(Span),
 }
 
 #[derive(Clone, Debug)]
@@ -2232,6 +2234,16 @@ impl Context {
                     ),
                 })
             }
+
+            Expression::ToDo(span) => Arc::new(process::Expression::Chan {
+                span: span.clone(),
+                captures: Captures::new(),
+                chan_name: LocalName::result(),
+                chan_annotation: None,
+                chan_type: (),
+                expr_type: (),
+                process: process::Process::todo(span.clone()),
+            }),
         });
         let None = self.original_object_name else {
             unreachable!("original_object_name should be none after expression")
@@ -2683,6 +2695,9 @@ impl Context {
                 }
                 ProcessStep::Command(command) => {
                     break self.compile_process_command(command, Some((source, index + 1)))?;
+                }
+                ProcessStep::ToDo(span) => {
+                    break process::Process::todo(span.clone());
                 }
             }
         };
@@ -3740,7 +3755,8 @@ impl<S> Spanning for Expression<S> {
             | Self::Neg { span, .. }
             | Self::ComparisonChain { span, .. }
             | Self::Application(span, _, _)
-            | Self::Construction(span, _) => span.clone(),
+            | Self::Construction(span, _)
+            | Self::ToDo(span) => span.clone(),
         }
     }
 }
@@ -3886,9 +3902,10 @@ impl<S> Spanning for Process<S> {
 impl<S> Spanning for ProcessStep<S> {
     fn span(&self) -> Span {
         match self {
-            Self::Let { span, .. } | Self::Catch { span, .. } | Self::If { span, .. } => {
-                span.clone()
-            }
+            Self::Let { span, .. }
+            | Self::Catch { span, .. }
+            | Self::If { span, .. }
+            | Self::ToDo(span) => span.clone(),
             Self::Command(command) => command.span.clone(),
         }
     }
--- crates/par-core/src/frontend_impl/parse.rs
+++ crates/par-core/src/frontend_impl/parse.rs
@@ -1643,6 +1643,7 @@ fn expression_without_construction(input: &mut Input) -> Result<Expression<Unres
         expr_do,
         expr_box,
         expr_chan,
+        expr_todo,
         infix_or,
     ))
     .context(StrContext::Label("expression"))
@@ -2770,6 +2771,7 @@ fn process_head(input: &mut Input) -> Result<(Span, ProcessHead)> {
         proc_compound_assign,
         proc_catch,
         proc_throw,
+        proc_todo,
         expression_command,
         global_command,
         command,
@@ -3023,6 +3025,23 @@ fn proc_throw(input: &mut Input) -> Result<(Span, ProcessHead)> {
                 )),
             )
         })
+        .parse_next(input)
+}
+
+fn proc_todo(input: &mut Input) -> Result<(Span, ProcessHead)> {
+    t(TokenKind::Todo)
+        .map(|token| {
+            (
+                token.span.clone(),
+                ProcessHead::Terminator(ProcessTerminator::ToDo(token.span.clone())),
+            )
+        })
+        .parse_next(input)
+}
+
+fn expr_todo(input: &mut Input) -> Result<Expression<Unresolved>> {
+    t(TokenKind::Todo)
+        .map(|token| Expression::ToDo(token.span.clone()))
         .parse_next(input)
 }
 
